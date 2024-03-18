c8564df147ab09f0fe3cf3d4333aa3676929e7d8
Mehedi Hasan <mehedihasan67705251@gmail.com>
2024-03-18 23:54:13 +0600

    Made projects section dynamic

--- src/pages/sections/portfolio/index.tsx
+++ src/pages/sections/portfolio/index.tsx
@@ -1,83 +1,40 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 // Plugins
-import { motion, AnimatePresence } from 'framer-motion';
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
+import { motion, AnimatePresence } from "framer-motion";
+import "reactjs-popup/dist/index.css";
 
-// UI Components
-import PortfolioItem1 from './items/PortfolioItem1';
-import PortfolioItem2 from './items/PortfolioItem2';
-import PortfolioItem3 from './items/PortfolioItem3';
-
-// Images
-import portfolio1 from '../../../assets/images/portfolio/portfolio1.jpg';
-import portfolio2 from '../../../assets/images/portfolio/portfolio2.jpg';
-import portfolio3 from '../../../assets/images/portfolio/portfolio3.jpg';
-import portfolio4 from '../../../assets/images/portfolio/portfolio4.jpg';
-import portfolio5 from '../../../assets/images/portfolio/portfolio5.jpg';
-import portfolio6 from '../../../assets/images/portfolio/portfolio6.jpg';
-import portfolio7 from '../../../assets/images/portfolio/portfolio6.jpg';
-// --> Portfolio items
-import portfolioItem1 from '../../../assets/images/portfolio/items/item_03.jpg';
-import portfolioItem2 from '../../../assets/images/portfolio/items/item_02.jpg';
 // --> Icon Images
-import backArrow from '../../../assets/images/close-left-arrow.png';
-import closeIcon from '../../../assets/images/close.png';
+import backArrow from "../../../assets/images/close-left-arrow.png";
 
 // Styles
-import './portfolio.css';
+import "./portfolio.css";
 
 // Data
-import portfolioData from '../../../data/portfolio.json';
-import { PortfolioItemType } from '../../../types/portfolio.types';
+
+import useData from "../../../Hook/useData";
+import PortfolioItem from "./items/PortfolioItem";
 
 // --------------
 
-function Portfolio() {
-  const images: string[] = [
-    portfolio1,
-    portfolio2,
-    portfolio3,
-    portfolio4,
-    portfolio5,
-    portfolio6,
-    portfolio7,
-  ];
-
+const Portfolio = () => {
   // Portfolio item to be shown (change rendered different components in item folder)
   const [portfolioItem, setPortfolioItem] = useState<number>(0);
   // Portfolio item to be shown as a popup
   const [openPortfolio, setOpenPortfolio] = useState<number>(0);
 
-  const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
-  const [selectedCategory, setSelectedCategory] = useState<string>('all');
+  const [isSeeAll, setIsSeeAll] = useState<boolean>(false);
+  const [selectedCategory, setSelectedCategory] = useState<string>("all");
+
+  // use hook to fatch dynamic data
+  const data = useData();
 
   /**
-   * Toggle filter buttons menu display
-   */
-  const handleToggleFilterBtns = () => {
-    setIsFilterOpen(!isFilterOpen);
-  };
-
-  /**
-   * Show images that have category matches the given, and
-   * remove the images with different category.
    *
-   * if the category equals `all`, it will show all images
-   *
-   * @param category images category to be shown
    */
-  const handleFilterImages = (category: string) => {
-    setSelectedCategory(category);
+  const handleSeeAllBtns = () => {
+    setIsSeeAll(!isSeeAll);
   };
-
-  const filteredImages: PortfolioItemType[] =
-    selectedCategory === 'all'
-      ? portfolioData.portfolioItems
-      : portfolioData.portfolioItems.filter(
-          (item) => item.category === selectedCategory
-        );
 
   /**
    * Opening portfolio item that the user clicked
@@ -86,7 +43,7 @@ function Portfolio() {
    */
   const handleOpenItem = (num: number) => {
     const element: HTMLElement | null =
-      document.getElementById('portfolio-wrapper');
+      document.getElementById("portfolio-wrapper");
     if (element) {
       element.scrollIntoView();
     }
@@ -102,20 +59,11 @@ function Portfolio() {
   };
 
   /**
-   * Open a popup of the item with the given number passed to the function
-   *
-   * @param num Pop up item number to be open
+   * find click project items from all projects
    */
-  const handleOpenPopup = (num: number) => {
-    setOpenPortfolio(num);
-  };
-
-  /**
-   * Closed the opened items by reseting the {openPortfolio} variable to 0
-   */
-  const handleClosePopup = () => {
-    setOpenPortfolio(0);
-  };
+  const clickedItem = data?.user?.projects?.find(
+    (item) => item.sequence == portfolioItem
+  );
 
   return (
     <section id="portfolio" className="section">
@@ -125,95 +73,103 @@ function Portfolio() {
             {portfolioItem === 0 ? (
               <>
                 <div
-                  className="category-filter"
-                  onClick={handleToggleFilterBtns}>
-                  <div className="category-filter-icon"></div>
-                </div>
-                <motion.div
-                  variants={{
-                    expanded: {
-                      height: 'auto',
-                      paddingTop: '24px',
-                      paddingBottom: '24px',
-                      opacity: 1,
-                      transition: {
-                        duration: 0.3,
-                      },
-                    },
-                    collapsed: {
-                      height: 0,
-                      paddingTop: 0,
-                      paddingBottom: 0,
-                      opacity: 1,
-                      transition: {
-                        duration: 0.3,
-                      },
-                    },
+                  className=""
+                  style={{
+                    display: "flex",
+                    justifyContent: "right",
+                    alignItems: "center",
                   }}
-                  animate={isFilterOpen ? 'expanded' : 'collapsed'}
-                  initial="collapsed"
-                  className={
-                    'category-filter-list button-group filters-button-group visible'
-                  }>
-                  {portfolioData.filter.map((flBtn, i) => (
-                    <div
-                      key={'filter-btn-' + i}
-                      className={
-                        'button ' +
-                        (selectedCategory === flBtn.category
-                          ? 'is-checked'
-                          : '')
-                      }
-                      onClick={() => handleFilterImages(flBtn.category)}>
-                      {flBtn.text}
-                    </div>
-                  ))}
-                </motion.div>
+                >
+                  <button
+                    onClick={handleSeeAllBtns}
+                    style={{
+                      paddingLeft: "20px",
+                      paddingRight: "20px",
+                      paddingTop: "10px",
+                      paddingBottom: "10px",
+                      background: "#F37B83",
+                      color: "white",
+                    }}
+                  >
+                    {isSeeAll ? "See Few" : "See All"}
+                  </button>
+                </div>
+
                 <div className="portfolio-load-content-holder"></div>
                 <motion.div className="grid" id="portfolio-grid" layout>
-                  {filteredImages.map((item, i) => (
-                    <AnimatePresence key={'portfolio-item-' + i}>
-                      <motion.div
-                        // layout
-                        animate={{ scale: 1, opacity: 1 }}
-                        initial={{ scale: 0, opacity: 0 }}
-                        exit={{ scale: 0, opacity: 0 }}
-                        transition={{ duration: 0.5 }}
-                        id={'p-item-' + (i + 1)}
-                        className="grid-item element-item p-one-third">
-                        <a
-                          className="item-link ajax-portfolio"
-                          style={{ position: 'relative' }}
-                          data-id={i + 1}
-                          onClick={() => {
-                            // according to action type we wil fire the function
-                            if (
-                              item?.action?.type === 'item' &&
-                              typeof item?.action?.number === 'number'
-                            ) {
-                              handleOpenItem(item.action.number);
-                            } else if (
-                              item?.action?.type === 'popup' &&
-                              typeof item?.action?.number === 'number'
-                            ) {
-                              handleOpenPopup(item.action.number);
-                            }
-                          }}>
-                          <img src={images[item.order]} alt="" />
-                          <div className="portfolio-text-holder">
-                            <div className="portfolio-text-wrapper">
-                              <p className="portfolio-text">
-                                {item.description.text}
-                              </p>
-                              <p className="portfolio-cat">
-                                {item.description.caption}
-                              </p>
+                  {isSeeAll ? (
+                    <>
+                      {data?.user?.projects?.map((item, i) => (
+                        <AnimatePresence key={"portfolio-item-" + i}>
+                          <motion.div
+                            // layout
+                            animate={{ scale: 1, opacity: 1 }}
+                            initial={{ scale: 0, opacity: 0 }}
+                            exit={{ scale: 0, opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                            id={"p-item-" + (i + 1)}
+                            className="grid-item element-item p-one-third"
+                          >
+                            <a
+                              className="item-link ajax-portfolio"
+                              style={{ position: "relative" }}
+                              data-id={i + 1}
+                              onClick={() => {
+                                // according to action type we wil fire the function
+
+                                handleOpenItem(item.sequence);
+                              }}
+                            >
+                              <img src={item.image.url} alt="" />
+                              <div className="portfolio-text-holder">
+                                <div className="portfolio-text-wrapper">
+                                  <p className="portfolio-text">{item.title}</p>
+                                  <p className="portfolio-cat">
+                                    {item.description}
+                                  </p>
+                                </div>
+                              </div>
+                            </a>
+                          </motion.div>
+                        </AnimatePresence>
+                      ))}
+                    </>
+                  ) : (
+                    data?.user?.projects?.slice(0, 6).map((item, i) => (
+                      <AnimatePresence key={"portfolio-item-" + i}>
+                        <motion.div
+                          // layout
+                          animate={{ scale: 1, opacity: 1 }}
+                          initial={{ scale: 0, opacity: 0 }}
+                          exit={{ scale: 0, opacity: 0 }}
+                          transition={{ duration: 0.5 }}
+                          id={"p-item-" + (i + 1)}
+                          className="grid-item element-item p-one-third"
+                        >
+                          <a
+                            className="item-link ajax-portfolio"
+                            style={{ position: "relative" }}
+                            data-id={i + 1}
+                            onClick={() => {
+                              // according to action type we wil fire the function
+
+                              handleOpenItem(item.sequence);
+                            }}
+                          >
+                            <img src={item.image.url} alt="" />
+                            <div className="portfolio-text-holder">
+                              <div className="portfolio-text-wrapper">
+                                <p className="portfolio-text">{item.title}</p>
+                                <p className="portfolio-cat">
+                                  {item.description}
+                                </p>
+                              </div>
                             </div>
-                          </div>
-                        </a>
-                      </motion.div>
-                    </AnimatePresence>
-                  ))}
+                          </a>
+                        </motion.div>
+                      </AnimatePresence>
+                    ))
+                  )}
                 </motion.div>
               </>
             ) : (
@@ -222,59 +178,19 @@ function Portfolio() {
                 <div
                   className="close-icon"
                   role="button"
-                  onClick={handlCloseItem}>
+                  onClick={handlCloseItem}
+                >
                   <img src={backArrow} alt="back arrow" />
                 </div>
-                {portfolioItem === 1 ? (
-                  <PortfolioItem1 />
-                ) : portfolioItem === 2 ? (
-                  <PortfolioItem2 />
-                ) : portfolioItem === 3 ? (
-                  <PortfolioItem3 />
-                ) : (
-                  <></>
-                )}
+
+                <PortfolioItem item={clickedItem} />
               </div>
             )}
           </div>
         </div>
       </div>
-
-      {/* Popups portfolio items */}
-      <Popup
-        open={openPortfolio !== 0}
-        closeOnDocumentClick
-        onClose={handleClosePopup}
-        modal>
-        <div className="my-popup">
-          <div
-            className="close-popup-btn"
-            role="button"
-            onClick={handleClosePopup}>
-            <img src={closeIcon} alt="close icon" />
-          </div>
-          {openPortfolio === 1 ? (
-            <p className="block-right poped-up-item" onClick={close}>
-              <iframe
-                src="https://player.vimeo.com/video/199192931"
-                width="100%"
-                allow="autoplay; fullscreen"></iframe>
-            </p>
-          ) : openPortfolio === 2 ? (
-            <div className="popup-image-box">
-              <img src={portfolioItem1} alt="portfolio image" />
-            </div>
-          ) : openPortfolio === 3 ? (
-            <div className="popup-image-box">
-              <img src={portfolioItem2} alt="portfolio image" />
-            </div>
-          ) : (
-            <></>
-          )}
-        </div>
-      </Popup>
     </section>
   );
-}
+};
 
 export default Portfolio;
---
+++ src/pages/sections/portfolio/items/PortfolioItem.tsx
@@ -0,0 +1,61 @@
+
+
+// Swiper Slider
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// Styles
+import './portfolio-item.css';
+
+// -------------------
+
+const PortfolioItem=({ item })=> {
+  return (
+    <div className="portfolio-item-wrapper">
+      <div className="portfolio-content">
+        <div className="row">
+          <div className="one-half width-55">
+            <div className="image-slider-wrapper relative block-right">
+              <Swiper
+                pagination={{ clickable: true }}
+                loop={true}
+                modules={[Pagination]}
+                className="portfolio-slider">
+                
+                  <SwiperSlide >
+                    <img src={item?.image?.url} alt={`portfolio item`} />
+                  </SwiperSlide>
+                
+              </Swiper>
+            </div>
+          </div>
+
+          <div className="one-half width-40 last">
+            <h2 className="entry-title section-title">{item.title}</h2>
+
+            <p className="section-info">{item.description}</p>
+
+            <p>
+              Cras pretium metus pulvinar ultricies auctor. In varius purus
+              blandit sem mollis tristique. Curabitur sed lorem vel ligula
+              pulvinar porttitor. 
+            </p>
+            <h6 className="">TechStack : </h6>
+            <ul>
+              {item.techStack?.map((item) => (
+                <li >&#x2022; {item}</li>
+              ))}
+            </ul>
+            <p style={{marginTop: "10px"}}>
+              <a className="button" href={item.liveurl} target="_blank" rel="noopener noreferrer">Check Project</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioItem;
